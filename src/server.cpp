4eaf93a03934550a22a412c16877bbd7d22a437e
Ambroise BAREUX <ambroise.bareux@gmail.com>
2026-03-16 22:45:09 +0100

    init

---
+++ src/server.cpp
@@ -0,0 +1,26 @@
+#include <server/udp.hpp>
+
+#include <iostream>
+
+std::string callback(std::string str) {
+    std::cout << "New message from client : " << str << std::endl;
+    return str;
+}
+
+std::atomic_bool run = true;
+
+std::string stop(std::string str) {
+    run = false;
+    return str;
+}
+
+int main() {
+    cookie::server::udp server(5000);
+
+    server.map_default(callback);
+    server.map_response_word("stop", stop);
+
+    server.start();
+
+    while (run);
+}
